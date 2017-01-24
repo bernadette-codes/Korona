4583f234cd0ab7d79aeddb0df01fb3bc4555b498
Bernadette <bernadette@englemaninvest.com>
2017-01-23 21:42:03 -0500

    Update korona.js

--- korona.js
+++ korona.js
@@ -7,14 +7,31 @@ $(document).ready(function(){
 
     var windowHeight = $(window).height(),
         windowWidth = $(window).width(),
-        myCarousel = $("#myCarousel");
+        $imgBig = $("#imgBig"),
+        $overlay = $("#overlay"),
+        $overlayContent = $("#overlayContent"),
+        $myCarousel = $("#myCarousel");
 
     // Window Height
     $(window).height(windowHeight+"px");
     //alert(windowWidth);
 
+    // Show Bigger Photos
+    $(".grid img").click(function(){
+        $imgBig.attr("src",$(this).prop('src'));
+        $overlay.show(350);
+        $overlayContent.show(350);
+    });
+
+    // Hide Bigger Photos
+    $imgBig.click(function(){
+        $(this).attr("src", "");
+        $overlay.hide();
+        $overlayContent.hide();
+    });
+
     // Carousel Photos
-    myCarousel.carousel({interval: 2000});
+    $myCarousel.carousel({interval: 2000});
 
     // Carousel Thumbnails
     $('[id^=carousel-selector-]').click(function() {
@@ -22,7 +39,7 @@ $(document).ready(function(){
         try {
             var id = /-(\d+)$/.exec(id_selector)[1];
             console.log(id_selector, id);
-            myCarousel.carousel(parseInt(id));
+            $myCarousel.carousel(parseInt(id));
         } catch (e) {
             console.log('Regex failed!', e);
         }
