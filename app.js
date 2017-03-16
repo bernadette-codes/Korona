fc3255da135d18e0fedd3d5bae2617d34c84006e
Bernadette <bernadette@englemaninvest.com>
2017-03-16 00:12:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
     app.controller('salonPhotosController', function($scope){
         $scope.photoTop = [
-            { alt: 'Salon1', photo: 'img/salon1.jpg'},
-            { alt: 'Salon2', photo: 'img/salon2.jpg'}
+            { alt: 'Salon1', photo: '../img/salon1.jpg'},
+            { alt: 'Salon2', photo: '../img/salon2.jpg'}
         ];
         $scope.photoBottom = [
-            { alt: 'Salon3', photo: 'img/salon3.jpg'},
-            { alt: 'Salon4', photo: 'img/salon4.jpg'}
+            { alt: 'Salon3', photo: '../img/salon3.jpg'},
+            { alt: 'Salon4', photo: '../img/salon4.jpg'}
         ];
     });
 
@@ -49,18 +49,18 @@
 
     app.controller('hairstylesController', function($scope){
         $scope.gallery = [
-            { name: 'Women Hairstyle Shoulder Length', photo: 'img/hairtsyle1.jpg', slideNum: 0, note: 'active item'},
-            { name: 'Women Hairstyle Shoulder Length', photo: 'img/hairtsyle2.jpg', slideNum: 1, note: 'item'},
-            { name: 'Women Hairstyle Shoulder Length', photo: 'img/hairtsyle3.jpg', slideNum: 2, note: 'item'},
-            { name: 'Women Hairstyle Long', photo: 'img/hairtsyle4.jpg', slideNum: 3, note: 'item'},
-            { name: 'Women Hairstyle Long', photo: 'img/hairtsyle5.jpg', slideNum: 4, note: 'item'},
-            { name: 'Women Hairstyle Long', photo: 'img/hairtsyle6.jpg', slideNum: 5, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle12.jpg', slideNum: 6, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle7.jpg', slideNum: 7, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle11.jpg', slideNum: 8, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle8.jpg', slideNum: 9, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle10.jpg', slideNum: 10, note: 'item'},
-            { name: 'Men Hairstyle', photo: 'img/hairtsyle9.jpg', slideNum: 11, note: 'item'}
+            { name: 'Women Hairstyle Shoulder Length', photo: '../img/hairtsyle1.jpg', slideNum: 0},
+            { name: 'Women Hairstyle Shoulder Length', photo: '../img/hairtsyle2.jpg', slideNum: 1},
+            { name: 'Women Hairstyle Shoulder Length', photo: '../img/hairtsyle3.jpg', slideNum: 2},
+            { name: 'Women Hairstyle Long', photo: '../img/hairtsyle4.jpg', slideNum: 3},
+            { name: 'Women Hairstyle Long', photo: '../img/hairtsyle5.jpg', slideNum: 4},
+            { name: 'Women Hairstyle Long', photo: '../img/hairtsyle6.jpg', slideNum: 5},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle12.jpg', slideNum: 6},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle7.jpg', slideNum: 7},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle11.jpg', slideNum: 8},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle8.jpg', slideNum: 9},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle10.jpg', slideNum: 10},
+            { name: 'Men Hairstyle', photo: '../img/hairtsyle9.jpg', slideNum: 11}
         ];
     });
 
